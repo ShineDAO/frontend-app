4eb63747a97072ea823e9cc4882d5af375972a6a
ShineDAO <shinedao@protonmail.com>
2021-05-12 11:48:59 +0200

    rebuild

--- docs/sw.js
+++ docs/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bb88843773375dcb66d3.js"
+    "url": "webpack-runtime-b38d9d36c0df3727dc05.js"
   },
   {
     "url": "framework-ee8a4bba4771daaa13e7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2bb784fe6c20e9609011b31ed81b4d46"
+    "revision": "836849ae8e502b19eb9f44ca37c2b165"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6b6b24e9d3742ef27043.js"
