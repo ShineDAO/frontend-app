3b46710afaa768000ac9aaa79a8575016812d9c4
obasilakis <vasileios.georgopoulos@gmail.com>
2021-05-26 20:40:00 +0300

    feat: import projects from json

--- src/components/dashboard/Projects/index.jsx
+++ src/components/dashboard/Projects/index.jsx
@@ -1,3 +1,4 @@
 import React from 'react';
+import * as projects from '../data/projects.json';
 
-export const Projects = () => <div>hey</div>;
+export const Projects = () => <div>{projects.projects}</div>;
