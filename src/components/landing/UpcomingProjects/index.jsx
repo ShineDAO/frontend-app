754016aafbbebe950167a5936ddc3c05dbf95a54
obasilakis <vasileios.georgopoulos@gmail.com>
2021-06-12 09:37:36 +0300

    feat: make upcoming projects responsive

--- src/components/landing/UpcomingProjects/index.jsx
+++ src/components/landing/UpcomingProjects/index.jsx
@@ -1,8 +1,18 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from 'providers/ThemeProvider';
-import { Card } from 'components/common';
 import DefiOptionsLogo from './defi_options_logo.png';
-import { HalfCircle, UpcomingContainer, HeaderWrapper, NameWrapper, Wrapper, UpcomingWrapper } from './styles';
+import {
+  HalfCircle,
+  UpcomingContainer,
+  HeaderWrapper,
+  NameWrapper,
+  Wrapper,
+  UpcomingWrapper,
+  TotalRaise,
+  FundingStage,
+  When,
+  BottomWrapper,
+} from './styles';
 import { Avatar } from '../../common/Avatar';
 import { Text } from '../../common/Text';
 
@@ -20,7 +30,7 @@ export const UpcomingProjects = () => {
               <Avatar imageUrl={DefiOptionsLogo} alt="Defi options logo" />
               <h2>Defi Options DAO</h2>
             </NameWrapper>
-            <Card background="#fada5e" borderRadius="4px" height="32px" width="140px">
+            <When>
               &nbsp; &nbsp;
               <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
                 <circle cx="4" cy="4" r="4" fill="#3F3D56" />
@@ -28,43 +38,22 @@ export const UpcomingProjects = () => {
               <Text color="#3F3D56" fontWeight={800}>
                 &nbsp; in 4 days
               </Text>
-            </Card>
+            </When>
           </HeaderWrapper>
-          <Card background="white">
-            <Card
-              alignItems="flex-start"
-              justifyContent="space-around"
-              background="#EEEEFF"
-              display="flex"
-              height="96px"
-              flexDirection="column"
-              margin="24px 24px 0 0"
-              padding="16px 0 16px 24px"
-              width="218px"
-            >
+          <BottomWrapper>
+            <TotalRaise>
               <Text color="#3F3D56">Total raise</Text>
               <Text color="#3F3D56" fontWeight={800}>
                 $200k
               </Text>
-            </Card>
-            <Card
-              alignItems="flex-start"
-              justifyContent="space-around"
-              background="white"
-              border="1px solid #3F3D56"
-              display="flex"
-              flexDirection="column"
-              height="96px"
-              margin="24px 0 0 0"
-              padding="16px 0 16px 24px"
-              width="218px"
-            >
+            </TotalRaise>
+            <FundingStage>
               <Text color="#3F3D56">Funding stage</Text>
               <Text color="#3F3D56" fontWeight={800}>
                 Seed Sale
               </Text>
-            </Card>
-          </Card>
+            </FundingStage>
+          </BottomWrapper>
         </UpcomingWrapper>
       </UpcomingContainer>
     </Wrapper>
--- src/components/landing/UpcomingProjects/styles.js
+++ src/components/landing/UpcomingProjects/styles.js
@@ -30,19 +30,38 @@ export const NameWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 265px;
-  @media (max-width: 960px) {
+  width: 270px;
+  @media (max-width: 495px) {
     flex-direction: column;
-    display: block;
+  }
+  h2 {
+    margin: 0 0 0 20px;
+    @media (max-width: 495px) {
+      margin: 16px 0 0 0;
+    }
   }
 `;
 
 export const UpcomingContainer = styled.div`
   margin: 0 auto;
   width: 1120px;
-
-  @media (max-width: 960px) {
+  @media (max-width: 1200px) {
     width: 70%;
+  }
+`;
+
+export const When = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  background: #fada5e;
+  border-radius: 4px;
+  height: 32px;
+  width: 140px;
+  @media (max-width: 699px) {
+    margin-top: 16px;
+    width: 218px;
+    justify-content: center;
   }
 `;
 
@@ -50,24 +69,73 @@ export const HeaderWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  flex-wrap: wrap;
   width: 460px;
   @media (max-width: 960px) {
     width: 100%;
-    flex-direction: column;
-    display: flex;
   }
+  @media (max-width: 699px) {
+    justify-content: center;
+  }
+`;
+
+export const BottomWrapper = styled.div`
+  justify-content: space-between;
+  display: flex;
+  flex-wrap: wrap;
+  background: white;
+  @media (max-width: 960px) {
+    width: 100%;
+  }
+  @media (max-width: 737px) {
+    justify-content: center;
+  }
+`;
+
+export const TotalRaise = styled.div`
+  align-items: flex-start;
+  justify-content: space-around;
+  background: #eeeeff;
+  display: flex;
+  height: 96px;
+  flex-direction: column;
+  margin: 24px 0 0 0;
+  padding: 16px 0 16px 24px;
+  width: 218px;
+  border-radius: 8px;
+`;
+
+export const FundingStage = styled.div`
+  align-items: flex-start;
+  justify-content: space-around;
+  background: white;
+  border: 1px solid #3f3d56;
+  display: flex;
+  flex-direction: column;
+  height: 96px;
+  margin: 24px 0 0 0;
+  padding: 16px 0 16px 24px;
+  width: 218px;
+  border-radius: 8px;
 `;
 
 export const UpcomingWrapper = styled.div`
   background: white;
   box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
-  display: block;
+  display: flex;
+  flex-direction: column;
   margin: 40px 0 0 0;
   padding: 40px;
+  border-radius: 8px;
   width: 540px;
   @media (max-width: 960px) {
     flex-direction: column;
     width: 100%;
+    padding: 40px calc((70vw - 460px) / 2);
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 40px;
   }
 `;
 
