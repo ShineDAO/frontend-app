c304afb170543eac29adbd1b7cfdbb0b37d0fbcc
ShineDAO <78757713+ShineDAO@users.noreply.github.com>
2022-05-27 18:00:34 +0000

    Deploying to gh-pages from @ ShineDAO/frontend-app@254b6744240675593b996501ebabffe0c49af28b 🚀

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-7ec4bcc52c210a4a5a05.js"
   },
   {
-    "url": "app-45e29336e7472700c568.js"
+    "url": "app-223f91b52e3ea5bdd116.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "da612171abff6972d75f6ba4f6c32093"
+    "revision": "3854e242f9452ebadcada1c516522b77"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0cdd30da6e98f04d1023.js"
@@ -139,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-45e29336e7472700c568.js`))) {
+  if (!resources || !(await caches.match(`/app-223f91b52e3ea5bdd116.js`))) {
     return await fetch(event.request)
   }
 
