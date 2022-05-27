254b6744240675593b996501ebabffe0c49af28b
ShineDAO <shinedao@protonmail.com>
2022-05-27 19:55:23 +0200

    update theme

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react';
 import useDarkMode from 'hooks/useDarkMode';
 
-export const ThemeContext = createContext('light');
+export const ThemeContext = createContext('dark');
 
 export default ({ children }) => {
 
