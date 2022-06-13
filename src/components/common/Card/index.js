56bbfe25f49eef511c7e0f6d80db3bf0f1817e75
BigCR7 <sergiosantos9966@gmail.com>
2022-06-13 04:44:21 -0500

    fix project-template

--- src/components/common/Card/index.js
+++ src/components/common/Card/index.js
@@ -1,18 +1,17 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-  background: ${({ background, theme }) => background || (theme === "light" ? "#fff" : "#181717")};
+  background: ${({ background }) => background || "#F3F669"};
   cursor: ${({ clickable }) => (clickable ? "pointer" : "default")};
   height: ${({ height }) => height || "100%"};
   width: ${({ width }) => width || "100%"};
   display: ${({ display }) => display || "flex"};
   align-items: ${({ alignItems }) => alignItems || "center"};
   justify-content: ${({ justifyContent }) => justifyContent || "center"};
-  border-radius: ${({ borderRadius }) => borderRadius || "8px"};
   box-shadow: ${({ boxShadow }) => boxShadow || "none"};
   flex-direction: ${({ flexDirection }) => flexDirection || "none"};
   flex-wrap: ${({ flexWrap }) => flexWrap || "none"};
-  border: ${({ border }) => border || "none"};
+  border: none;
   padding: ${({ padding }) => padding || "0"};
   margin: ${({ margin }) => margin || "0"};
 `;
