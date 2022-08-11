48937b16611d68ae03be5de54d46fc52d93eefdb
ShineDAO <shinedao@protonmail.com>
2022-08-11 11:16:41 +0200

    update

--- src/pages/seed.js
+++ src/pages/seed.js
@@ -12,13 +12,29 @@ import { useQueryParam } from "../hooks/useQueryParam";
 
 export default ({}) => {
   const [activeContract, setActiveContract] = useQueryParam(`tag`);
+  const { isWalletEnabled, chainId } = useContext(WalletContext);
 
   return (
     <Layout position="absolute" bottom="4px" width="100%" height="60px">
       <SEO title="seed" description="Launch seed" />
       <Header />
       <MobileDiv mobileWidth="100%" width="90%" background="" style={{ display: "flex", alignItems: "center", flexDirection: "column", paddingBottom: "200px" }}>
-        <SeedContainer activeContract={activeContract} setActiveContract={setActiveContract}></SeedContainer>
+        {!isWalletEnabled && <h3 style={{ paddingTop: 80 }}>Please connect your wallet to see and create deals.</h3>}
+
+        {chainId == "0x13881" || (process.env.NODE_ENV == "development" && chainId == "0x7a69") ? (
+          <SeedContainer activeContract={activeContract} setActiveContract={setActiveContract}></SeedContainer>
+        ) : (
+          isWalletEnabled && (
+            <div style={{ textAlign: "center" }}>
+              <br></br>
+              <Text style={{ marginTop: 80 }} fontWeight="600">
+                Chain unsupported
+              </Text>
+              <br></br>
+              <p style={{ marginLeft: 80, marginRight: 80 }}>App is only available on Mumbai Network.</p>
+            </div>
+          )
+        )}
       </MobileDiv>
     </Layout>
   );
