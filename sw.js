0078460e9d7208b9fba2e8549bc42b746a0a18fd
ShineDAO <78757713+ShineDAO@users.noreply.github.com>
2022-09-07 15:32:23 +0000

    Deploying to gh-pages from @ ShineDAO/frontend-app@ffa044faaa6c00cac5eafc15f69bde0e0e937e55 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-045d403c8f1f50cbea0a.js"
+    "url": "webpack-runtime-fd3ace29a4290bb52917.js"
   },
   {
     "url": "framework-308dad6a7f2f8b850c29.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6597d7a6735b47cab5ccfd47bfed8172"
+    "revision": "fe7f3eafb9803a47ffcb95ba1db1b152"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0cdd30da6e98f04d1023.js"
